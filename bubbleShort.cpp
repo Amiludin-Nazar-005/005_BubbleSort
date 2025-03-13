c86f133ab25adec033dde1895430dd3b0158810b
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-03-13 11:47:10 +0700

    prosedur display

--- bubbleShort.cpp
+++ bubbleShort.cpp
@@ -43,5 +43,26 @@ void bubbleSortArray(){
         pass = pass +1; //step 4
     }while (pass <= n -1);//step 5
 }
+void display (){
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "element array yang telah tersusun" << endl;
+    cout << "====================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j];//menampilkan array
+        if (j < n -1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "jumlah pass = " << n -1 << endl; //menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+
+    
+}
 
     
